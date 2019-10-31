72a8c81e933b575fa39169601408a73824de0c4c
kyjm2010 <kyjm2010@gmail.com>
2019-10-31 15:14:14 -0700

    Demo 1031 Happy Halloween Bill!

---
+++ ConsoleApp1/MyMath1.UnitTest/MyMath1Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyMath1;
+using NUnit.Framework;
+
+namespace MyMath1.UnitTest
+{
+    [TestFixture]
+    public class MyMath1Tests
+    {
+        [Test]
+        public void Add_SumWithinByteRange_ReturnCorectSum()
+        {
+            Assert.AreEqual(200, MyMath.MyMath1.Add(101, 99));
+
+        }
+        [Test]
+        public void Add_SumOutsideByteRange_ReturnIncorectSum()
+        {
+            Assert.AreEqual(302, MyMath.MyMath1.Add(101, 201));
+        }
+        [Test]
+        public void Add_SumWithinByteRange_AlternateAssert()
+        {
+            Assert.That(MyMath.MyMath1.Add(101, 99), Is.EqualTo(200));
+        }
+        [Test]
+        public void Add_SumOutsideByteRange_ThrowsException()
+        {
+            Assert.That(() => MyMath.MyMath1.Add(101, 201), Throws.Exception);
+        }
+    }
+}
